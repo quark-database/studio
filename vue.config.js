271373aed1a45d90d36c6ba86515a3216eded757
Анатолий <contact@anafro.ru>
2022-10-29 20:20:20 +1000

    🔥 Added `electron-renderer`

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  configureWebpack: {
+    target: 'electron-renderer'
+  },
   transpileDependencies: true
 })
