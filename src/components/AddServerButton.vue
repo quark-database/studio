9bafef6fe285b60b429349c9d264ba006d8dd944
Анатолий <contact@anafro.ru>
2022-10-29 20:16:54 +1000

    🏃‍♂️ Added an animation

--- src/components/AddServerButton.vue
+++ src/components/AddServerButton.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-img#add-server-button(:src="buttonImagePath")
+img#add-server-button(:src="buttonImagePath", v-bind:class="{'pulsing': isPulsing}")
 </template>
 
 <script>
@@ -8,14 +8,24 @@ export default {
   data() {
     return {
       buttonImagePath: "/icons/server-page/add-server-button.svg",
+      isPulsing: true,
     }
   }
 }
 </script>
 
 <style lang="sass" scoped>
+@import "../assets/colors"
+
 #add-server-button
   width: 30px
   height: auto
   cursor: pointer
+  &.pulsing
+    animation: add-server-button-animation infinite alternate-reverse 300ms ease-in-out
+    @keyframes add-server-button-animation
+      from
+        transform: scale(.8)
+      to
+        transform: scale(1.3)
 </style>
