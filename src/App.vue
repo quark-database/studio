0894d7c31cb041cf5ef94ff939f4c3cc905434e2
Анатолий <contact@anafro.ru>
2022-10-29 20:17:30 +1000

    ⚙️ Implemented sidebar and content panel functionality

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 ToolBar
 #bottom-components-container
-  SideBar
-  ContentPanel
+  SideBar(ref="sideBar", v-on:sidebar-option-selected="sidebarOptionSelected($event)")
+  ContentPanel(ref="contentPanel", v-on:connected="onConnection")
 </template>
 
 <script>
@@ -16,18 +16,50 @@ export default {
     ToolBar,
     SideBar,
     ContentPanel
+  },
+
+  data() {
+    return {
+      currentServer: {
+        host: "localhost",
+        port: 10000,
+        token: "root"
+      }
+    }
+  },
+
+  methods: {
+    sidebarOptionSelected(sidebarOption) {
+      this.$refs.contentPanel.switchPanelContent(sidebarOption)
+    },
+
+    onConnection() {
+      this.$refs.sideBar.connectedToServer = true
+    }
   }
 }
 </script>
 
 <style lang="sass">
+@import "assets/colors"
+
+h1, h2, h3, h4, h5, h6
+  display: block
 html, body
   height: 100%
   width: 100%
   margin: 0
   padding: 0
+  overflow: hidden
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
+  *::selection
+    background: $primary
+    color: white
+
+a
+  color: $primary
+  text-decoration: none
 
 #app
   font-family: Gilroy, "Helvetica Neue", Helvetica, Arial, sans-serif
@@ -36,8 +68,11 @@ html, body
   display: flex
   flex-flow: column
   height: 100%
+  overflow: hidden
   #bottom-components-container
     display: flex
     align-items: stretch
-    height: calc(100% - 20px)
+    min-height: calc(100% - 20px)
+    max-height: calc(100% - 20px)
+
 </style>
