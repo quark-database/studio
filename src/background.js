25e4cd9a54d242c7e20206c971f9e34c82f99810
Анатолий <contact@anafro.ru>
2022-10-29 20:17:59 +1000

    ⚙️ Added Electron IPC handlers

--- src/background.js
+++ src/background.js
@@ -1,9 +1,15 @@
 'use strict'
 
-import { app, protocol, BrowserWindow, ipcMain } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
+import {app, BrowserWindow, ipcMain, protocol} from 'electron'
+import {createProtocol} from 'vue-cli-plugin-electron-builder/lib'
+import installExtension, {VUEJS3_DEVTOOLS} from 'electron-devtools-installer'
+import {askQuarkusFor} from "@/scripts/shared/quarkus";
+import {Client} from "quark-api"
+import * as fs from "fs";
+import * as StringSimilarity from "string-similarity";
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
+const serversFile = 'servers.json';
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
@@ -15,20 +21,21 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     icon: __dirname + '/../public/logo.ico',
+    title: "Quark Studio 1.1",
     webPreferences: {
-      
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
+
+  win.maximize()
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    // if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
@@ -66,6 +73,8 @@ app.on('ready', async () => {
   createWindow()
 })
 
+let server = null
+
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
@@ -81,6 +90,113 @@ if (isDevelopment) {
   }
 }
 
-ipcMain.on('ask-server-list', event => {
-  event.frameId
+function getServerList() {
+  return new Promise(resolve => {
+
+    if(!fs.existsSync(serversFile)) {
+      resolve([])
+    }
+
+    fs.readFile(serversFile, (error, content) => {
+      if(error) {
+        console.error(content)
+        resolve([])
+      } else {
+        resolve(JSON.parse(content.toString()).list)
+      }
+    })
+  })
+}
+
+function updateServerList(serverListConsumer) {
+  return new Promise(resolve => {
+    getServerList().then(servers => {
+      serverListConsumer(servers)
+      fs.writeFile(serversFile, JSON.stringify({ "list": servers }, null, 4), () => {});
+      resolve()
+    });
+  })
+}
+
+ipcMain.handle('check-connection', (event, host, port, token) => {
+  return new Promise(resolve => {
+    const client = new Client(token, host, port);
+    client.query("list databases;").then(() => {
+      resolve("online");
+    }).catch(() => {
+      resolve("offline");
+    });
+  })
+})
+
+ipcMain.handle('add-server', (event, host, port, token) => {
+  return new Promise(resolve => {
+    updateServerList(servers => {
+      servers.push({
+        host: host,
+        port: port,
+        token: token
+      })
+
+      resolve()
+    }).then(resolve)
+  })
+})
+
+ipcMain.handle('delete-server', (event, host, port, token) => {
+  return new Promise(resolve => {
+    updateServerList(servers => {
+      console.log(servers)
+
+      servers.splice(servers.indexOf({
+        host: host,
+        port: port,
+        token: token
+      }), 1)
+    }).then(resolve)
+  })
+})
+
+ipcMain.handle('ask-server-list', () => {
+  return getServerList();
+})
+
+ipcMain.handle('ask-quarkus', (event, message) => {
+  return askQuarkusFor(message)
+})
+
+ipcMain.handle('ask-table-list', (event, requestingServer) => {
+  return new Promise(resolve => {
+    if(requestingServer === undefined) {
+      requestingServer = server
+    }
+
+    const client = new Client(requestingServer.token, requestingServer.host, requestingServer.port);
+    client.query("list databases;").then(listDatabasesResult => {
+
+      Promise.all(listDatabasesResult.tableView.rows.map(row => row.cells[0]).map(databaseName => client.query(`list tables in "${databaseName}";`).then(listTablesInResult => {
+        return listTablesInResult.tableView.rows.map(row => row.cells[0]).map(tableName => databaseName + "." + tableName)
+      }))).then(list => resolve(list.flat()));
+    });
+  });
+});
+
+ipcMain.handle('run-query', (event, query, requestingServer) => {
+  if(requestingServer === undefined) {
+    requestingServer = server
+  }
+
+  return new Client(requestingServer.token, requestingServer.host, requestingServer.port).query(query);
+})
+
+ipcMain.handle('find-best-string', (event, string, options) => {
+  return StringSimilarity.findBestMatch(string, options);
+})
+
+ipcMain.handle('get-server', () => {
+  return server
+})
+
+ipcMain.handle('set-server', (event, newServer) => {
+  server = newServer
 })
